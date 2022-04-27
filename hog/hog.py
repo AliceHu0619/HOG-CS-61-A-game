3f5c09c0449f620605979bd101ee582694d7d16d
Hu's GitHub <alicehu0131@gmail.com>
2022-04-27 21:21:15 +0800

    Update hog.py

--- hog/hog.py
+++ hog/hog.py
@@ -69,4 +69,83 @@ def silence(score0, score1):
 
 
 
+def play(strategy0, strategy1, score0 = 0, score1 = 0, dice = six_sided, goal
+= GOAL_SCORE, say = silence );
+	player = 0
+	while True:
+		if score0 >= goal or score1 > goal:
+			break
+		if player == 0:
+			num = strategy0(score0, score1)
+			score0 += take_turn(num, score1,dice)
+		else:
+			num = strategy1(score1, score0)
+			score1 += take_turn(num, score0, dice)
+
+		if is_swap(score0, score1):
+			score0, score1 = score1, score0
+
+		player = other(player)
+		say = say(score0, score1)
+
+	return score0, score1
+
+
+
+
+def say_scores(score0, score1):
+	print('player 0 now has', score0, 'and player1 now has', score1)
+	return say_scores
+
+
+def announce_lead_changes(previous_leader = None):
+	>>> f0 = announce_lead_changes()
+	>>> f1 = f0(5, 0)
+	>>> f2 = f1(5,12)
+	>>> f3 = f2(8,12)
+	>>> f4 = f3(8, 13)
+	>>> f4 = f4(15,13)
+
+
+	def say(score0, score1):
+		if score0 > score1:
+			leader = 0
+		elif score1 > score0:
+			leader = 1
+		else:
+			leader = None
+
+		if leader != None and leader != previous_leader:
+			print('player', leader, 'take the lead by', abs(score0- score1))
+
+		return announce_lead_changes(leader)
+
+	return say
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
